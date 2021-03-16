59b142059a7d2d512f29b24753856fb302118e63
ThalitaProgramadora <thalita.programadora@gmail.com>
2021-03-16 09:49:44 -0300

    atualizando

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,6 @@ por Id:getElementById()
 por Nome:getElementsByName()
 por Classe:getElementsByClassName()
 por Seletor:querySelector()
-*/
-//let nome = window.document.getElementById('nome')
-/*
 se for id coloca # se for classe . dentro do parenteses.
 pode usar #assunto ou input#assunto 
 */
@@ -17,21 +14,23 @@ let assunto = document.querySelector('#assunto')
  /* 
  string é um array aqui.
  */
-
 function validaNome(){
     let txtNome = document.querySelector('#txtNome')
-    if(nome.Value.length < 3){
+    if(nome.value.length < 3){
         txtNome.innerHTML = 'nome inválido!!'
         txtNome.style.color = 'red'
     }else{
-        txtNome.innerHTML = 'Nome Inválido!!'
-        txtNome.style.color = 'green'
+        txtNome.innerHTML = 'Nome correto!!'
+        txtNome.style.color = 'black'
     }
 }
 function validaEmail(){
     let txtEmail = document.querySelector('#email')
-    if(email.Value.indexOf('@') -- -1){
-        txtEmail.innerHTML = 'Email invalido'
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+       // txtEmail.innerHTML = 'nome inválido!!'
         txtEmail.style.color =  'red'
+    }else{
+      //  txtEmail.innerHTML = 'Email correto!!'
+        txtEmail.style.color =  'green'
     }
 }
